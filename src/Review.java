96495ace6697aa7e532697fcd235643a89447177
Ibrahim Qatato <ibrahimq1998@gmail.com>
2020-12-03 16:03:06 +0100

    code review changes for grep

--- src/Review.java
+++ src/Review.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class Review {
 	public static void main(String[] args) {
+		if (args.length<2) {
+			System.out.println("requires <pattern> <file>");
+		}
 		String pattern = args[0];
 		File file = new File(args[1]);
 		if (!file.exists()) {
-			System.out.println("file doesn't exist make sure to inpute the correct "
+			System.out.println("file doesn't exist make sure to input the correct "
 					+ "arguments, <pattern> <file>");
 			return;
 		}
